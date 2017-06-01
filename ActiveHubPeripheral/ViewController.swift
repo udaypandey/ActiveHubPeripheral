4c1bba743801bb0e749bb05213a824b4b2a0b406
Uday Pandey <uday.pandey@hivehome.com>
2017-06-01 13:33:49 +0100

    HIOS-7338: Sample Active Hub Peripheral for Hive Sense

---
+++ ActiveHubPeripheral/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ActiveHubPeripheral
+//
+//  Created by Uday Pandey on 01/06/2017.
+//  Copyright © 2017 Centrica Connected Home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var activeHubDevice =  DMActiveHubDevice()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        activeHubDevice.startAdvertising()
+    }
+}
+
